f6c4965f2adf419fca020a94274d0beef5585d3a
KOHVPS99 <fadi.hardan.family@gmail.com>
2026-03-12 02:57:41 +0400

    Update finnhub.js

--- services/finnhub.js
+++ services/finnhub.js
@@ -2,22 +2,25 @@ const axios = require("axios");
 
 async function getPrice(symbol) {
 
+  const now = Math.floor(Date.now() / 1000);
+  const from = now - 60;
+
   const res = await axios.get(
-    "https://finnhub.io/api/v1/quote",
+    "https://finnhub.io/api/v1/stock/candle",
     {
       params: {
         symbol: symbol,
+        resolution: 1,
+        from: from,
+        to: now,
         token: process.env.FINNHUB_API_KEY
-      },
-      headers: {
-        "Cache-Control": "no-cache"
       }
     }
   );
 
-  if (!res.data || res.data.c === 0) return null;
+  if (!res.data || !res.data.c || res.data.c.length === 0) return null;
 
-  return res.data.c;
+  return res.data.c[res.data.c.length - 1];
 }
 
 module.exports = { getPrice };
